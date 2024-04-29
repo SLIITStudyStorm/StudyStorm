63a2c3dafebc1de62ce35f3e0daf34f3b87b3cae
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2024-04-30 01:01:50 +0530

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,19 @@ import Header from './components/header';
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [title, setTitle] = useState("Home");
   const location = useLocation();
-  const isHomePage = location.pathname === '/';
+  const [title, setTitle] = useState("Home");
+  const [showHeader, setShowHeader] = useState(location.pathname === '/');
 
   useEffect(() => {
     switch (location.pathname) {
       case '/':
         setTitle('Home');
+        setShowHeader(true);
         break;
       default:
         setTitle('404 Not Found');
+        setShowHeader(true);
         break;
     }
 
@@ -27,7 +29,7 @@ const App = () => {
     <>
       <ToastContainer />
       <Container className='mw-100 py-0 px-0' id="main" style={{position:'fixed', minHeight:'100vh', width:'100%', height:'100%', overflow:'auto', display:'flex', background:'rgb(246 245 238)'}}>
-        {isHomePage ? <Header /> : <Sidebar />}        
+        {showHeader ? <Header /> : <Sidebar />}        
         <Outlet />
       </Container>
     </>
